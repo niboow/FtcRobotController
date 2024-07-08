d537077a5596749982f23208ca871048be11ca17
Woobin <woobinwang07@gmail.com>
2024-07-08 12:57:47 -0700

    nathan android studio NOT buttcheeks

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/servo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/servo.java
@@ -1,31 +1,41 @@
 package org.firstinspires.ftc.teamcode.OpModes;
 
-
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
-@TeleOp
+@TeleOp(name="Servo Test", group="Linear Opmode")
 public class servo extends LinearOpMode {
-    public CRServo servo;
+    private CRServo servo;
 
     @Override
     public void runOpMode() throws InterruptedException {
-
+        // Initialize the servo
         servo = hardwareMap.get(CRServo.class, "test");
         servo.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        // Send telemetry message to signify robot waiting
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        // Wait for the game to start (driver presses PLAY)
         waitForStart();
 
-        if (isStopRequested()) return;
-
-        while(opModeIsActive()){
+        // Run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+            // Set servo power
             servo.setPower(1);
+
+            // Display the current servo power
+            telemetry.addData("Servo Power", servo.getPower());
+            telemetry.update();
+
+            // Sleep for a short period to avoid running the loop too fast
+            sleep(100);
         }
+
+        // Stop the servo
+        servo.setPower(0);
     }
-    //testing everytioasdfkhasdf
-    //testing push
-    //testing push clone
 }
