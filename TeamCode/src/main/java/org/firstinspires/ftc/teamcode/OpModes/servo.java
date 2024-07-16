eba5eb1001e16b3c9ac6ac319c39e718e1c9370c
Nathan Xie <107293298+NathanXie6@users.noreply.github.com>
2024-07-15 18:42:15 -0700

    testing encoder

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/servo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/servo.java
@@ -3,8 +3,10 @@ package org.firstinspires.ftc.teamcode.OpModes;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import android.util.Log;
 import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp
@@ -14,19 +16,51 @@ public class servo extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         servo = hardwareMap.get(CRServo.class, "test");
-        DcMotor frontLeftMotor = hardwareMap.dcMotor.get("frontLeftMotor");
-        DcMotor testMotor = hardwareMap.dcMotor.get("testMotor");
-        DcMotor poopMotor = hardwareMap.dcMotor.get("poopMotor");
+
+
+        DcMotorEx frontLeftMotor = hardwareMap.get(DcMotorEx.class, "frontLeftMotor");
+        frontLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontLeftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        DcMotorEx testMotor = hardwareMap.get(DcMotorEx.class, "testMotor");
+        DcMotorEx poopMotor = hardwareMap.get(DcMotorEx.class, "poopMotor");
 
         servo.setDirection(DcMotorSimple.Direction.REVERSE);
         waitForStart();
         if (isStopRequested()) return;
+
+            double prev = 0;
+            double prevPos = 0;
         while(opModeIsActive()){
            // servo.setPower(0.5);
+            frontLeftMotor.setVelocity(0.1);
             frontLeftMotor.setPower(0.1);
+
             testMotor.setPower(0.1);
             poopMotor.setPower(0.1);
 
+            double velo = frontLeftMotor.getVelocity();
+            double velo2 = testMotor.getVelocity();
+
+            Log.i("velocity", String.valueOf(velo));
+            //code for deriving velocity from position (old and not used)
+//            double now = System.currentTimeMillis();
+//
+//            frontLeftMotor.setPower(0.1);
+//            int newPos = frontLeftMotor.getCurrentPosition();
+//
+//            testMotor.setPower(0.1);
+//            poopMotor.setPower(0.1);
+//
+//            double elMillis =  now - prev;
+//            double elSec = elMillis / 1000;
+//            double elPos = newPos - prevPos;
+//            double velocity = elPos/ elSec;
+//
+//            prev = now;
+//            prevPos = newPos;
+//
+//            Log.i("elTime", String.valueOf(elPos));
 
 //            double time_curr = System.currentTimeMillis();
 //            double time_new = 0;
